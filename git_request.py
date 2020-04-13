86a6a8d9d0c905202e7c07fbe20880f9c57cf05f
Tao Ling Jiang <358693114@qq.com>
2020-04-13 12:50:52 +0800

    finish cn tour

--- git_request.py
+++ git_request.py
@@ -1,6 +1,18 @@
 import requests
+from bs4 import BeautifulSoup
 
 url="http://www.cntour.cn"
 
 response=requests.get(url)
 print(response.text)
+
+soup=BeautifulSoup(response.text,'lxml')
+date=soup.select("#main > div > div.mtop.firstMod.clearfix > div.leftBox > "
+                 "div:nth-child(2) > ul > li:nth-child(5) > a")
+print(date)
+for item in date:
+    result={
+        'title':item.get_text(),
+        'link':item.get('href')
+    }
+print(result)
